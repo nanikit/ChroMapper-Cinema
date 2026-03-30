2957f6469445b054b6124ce8a01277fe36a4a9c2
nanikit <nanikit4@gmail.com>
2026-03-31 02:34:14 +0900

    style: format

--- ChroMapper-Cinema/Cinema.cs
+++ ChroMapper-Cinema/Cinema.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 using UnityEngine.Video;
 using SimpleJSON;
 
-namespace ChroMapper_Cinema {
+namespace ChroMapper_Cinema;
 
 public class Cinema {
 	public ExtensionButton main_button;
 	private AudioTimeSyncController atsc;
-	
+
 	private bool enabled;
 	private GameObject parent;
 	private GameObject screen;
@@ -18,28 +18,28 @@ public class Cinema {
 	private CustomBloom disabledCustomBloomSettings;
 	private bool disabledCustomBloomPreviousActive;
 	private bool disabledCustomBloomPreviousEnabledValue;
-	
+
 	private float offset;
 	private bool playing;
-	
+
 	public Cinema() {
 		main_button = ExtensionButtons.AddButton(
 			Utils.LoadSprite("ChroMapper_Cinema.Resources.Icon.png"),
 			"Cinema",
 			ToggleEnabled);
 	}
-	
+
 	public void Init(AudioTimeSyncController atsc, GameObject platform) {
 		this.parent = platform;
 		this.atsc = atsc;
-		
+
 		this.platform = platform.name.Replace("(Clone)", "");
 		Debug.Log($"Platform: {this.platform}");
-		
+
 		screen = new GameObject("Cinema Screen");
 		screen.transform.SetParent(parent.transform);
 		screen.SetActive(false);
-		
+
 		var mesh = new Mesh();
 		mesh.name = "Scripted_Plane_New_Mesh";
 		mesh.vertices = new Vector3[] { Utils.V3(-0.5f, -0.5f, 0.01f), Utils.V3(0.5f, -0.5f, 0.01f), Utils.V3(0.5f, 0.5f, 0.01f), Utils.V3(-0.5f, 0.5f, 0.01f) };
@@ -47,11 +47,11 @@ public class Cinema {
 		mesh.triangles = new int[] { 2, 1, 0, 3, 2, 0 };
 		mesh.RecalculateNormals();
 		screen.AddComponent<MeshFilter>().mesh = mesh;
-		
+
 		var mat = new Material(Shader.Find("UI/Default"));
 		mat.color = Color.white;
 		screen.AddComponent<MeshRenderer>().material = mat;
-		
+
 		player = screen.AddComponent<VideoPlayer>();
 		player.errorReceived += (VideoPlayer p, string msg) => {
 			enabled = false;
@@ -65,19 +65,19 @@ public class Cinema {
 			Debug.Log("Cinema prepared: " + (p.isPrepared ? "true" : "false"));
 			enabled = true;
 			playing = false;
-			
+
 			Update();
 		};
 		player.seekCompleted += AfterSeek;
 		player.playOnAwake = true;
 		player.audioOutputMode = VideoAudioOutputMode.None;
-		
+
 		LoadVideo();
-		
+
 		atsc.OnTimeChanged += Update;
 		Settings.NotifyBySettingName("SongSpeed", UpdateSongSpeed);
 	}
-	
+
 	public string LoadVideo() {
 		var map_dir = BeatSaberSongContainer.Instance.Info.Directory;
 		var cinema_file = Path.Combine(map_dir, "cinema-video.json");
@@ -86,11 +86,11 @@ public class Cinema {
 			enabled = false;
 			return Utils.Error("No cinema-video.json!");
 		}
-		
+
 		StreamReader reader = new StreamReader(cinema_file);
 		var cinema_info = JSONNode.Parse(reader.ReadToEnd()).AsObject;
 		reader.Close();
-		
+
 		Options.instance.LoadSettings();
 		plat_settings = Options.instance.GetPlatformSettings(platform);
 		if (cinema_info.HasKey("screenPosition")) {
@@ -102,31 +102,31 @@ public class Cinema {
 		if (cinema_info.HasKey("screenHeight")) {
 			plat_settings.height = cinema_info["screenHeight"].AsFloat;
 		}
-		
+
 		offset = ((int)cinema_info["offset"]) / 1000.0f;
-		
+
 		var mapFolderName = new DirectoryInfo(map_dir).Name;
 		var wipDir = Directory.GetParent(map_dir).FullName;
 		var videoPath = Path.Combine(wipDir, "CinemaWIPVideos", mapFolderName, (string)cinema_info["videoFile"]);
-		
+
 		if (!File.Exists(videoPath)) {
 			videoPath = Path.Combine(map_dir, (string)cinema_info["videoFile"]);
 		}
-		
+
 		if (!File.Exists(videoPath)) {
 			RestoreCustomBloom();
 			return Utils.Error("Video file not downloaded!");
 		}
-		
+
 		screen.SetActive(true);
 		DisableCustomBloomForCinema();
-		
+
 		player.url = videoPath;
 		player.Prepare();
-		
+
 		return "";
 	}
-	
+
 	public void ToggleEnabled() {
 		if (enabled) {
 			player.Stop();
@@ -141,34 +141,34 @@ public class Cinema {
 			}
 		}
 	}
-	
+
 	private void Update() {
 		if (!enabled) return;
-		
+
 		var time = atsc.CurrentSeconds + offset;
-		
+
 		// Causes lag when playing
 		if (!playing) {
 			player.time = time;
 		}
-		
+
 		if (atsc.IsPlaying && !playing && time >= 0) {
 			player.Play();
 			playing = true;
 		}
-		
+
 		if (!atsc.IsPlaying && playing) {
 			player.Pause();
 			playing = false;
 		}
 	}
-	
+
 	private void UpdateSongSpeed(object obj) {
 		if (!enabled) return;
-		
+
 		player.playbackSpeed = ((float)obj) / 10.0f;
 	}
-	
+
 	private void AfterSeek(VideoPlayer player) {
 		if (!playing) {
 			player.StepForward();
@@ -198,6 +198,4 @@ public class Cinema {
 		disabledCustomBloomSettings.enabled.value = disabledCustomBloomPreviousEnabledValue;
 		disabledCustomBloomSettings = null;
 	}
-}
-
 }
